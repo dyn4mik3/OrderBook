379b9e50570c0d67c69adacbc0a25e17c19bbe43
Michael Nguyen <dyn4mik3@gmail.com>
2013-12-31 00:14:52 -0800

    removed test

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from distutils.core import setup
 
 setup(
     name='OrderBook',
-    version='0.1',
+    version='0.1.2',
     author='Michael Nguyen',
     author_email='dyn4mik3@gmail.com',
     packages=['orderbook'],
     url='http://pypi.python.org/pypi/OrderBook/',
     license='LICENSE.txt',
-    description='Limit Order Book Trading Engine',
-    long_description=open('README.txt').read(),
+    description='Limit Order Book Matching Engine',
     install_requires=[
         "bintrees >= 2.0.1"
         ],
